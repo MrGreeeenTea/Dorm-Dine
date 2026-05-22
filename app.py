b77b523bc8787d93c37a84f552a24d70f293e067
A-Ye311 <angelina.musik@live.de>
2026-05-22 09:56:37 +0200

    url + html

--- app.py
+++ app.py
@@ -1,46 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 # landing page
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return redirect(url_for('index'))
 
 # feed von den meals
 @app.route('/feed')
 def feed():
-    return 'Hello, World!'
+    return redirect(url_for('feed'))
 
 # einzelne Gerichte
 @app.route('/meal/<int:meal_id>')
 def meal_detail(meal_id):
-    return
+    return redirect(url_for('meal'))
 
 # Gerichte posten
 @app.route('/post', methods=['GET', 'POST'])
 def post_meal():
-    return
+    return redirect(url_for('post'))
 
 # Profil anzeigen
 @app.route('/profile/<int: profil_id>')
 def profil():
-    return
+    return redirect(url_for('profile'))
 
 # Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return
+    return redirect(url_for('login'))
 
 # Register
 @app.route('/register' , methods=['GET', 'POST'])
 def register():
-    return
+    return redirect(url_for('register'))
 
 # Bestellübersicht
 @app.route('/order_view')
 def order_view():
-    return
+    return redirect(url_for('order_view'))
 
 # 
 @app.errorhandler(404)
