0d399c25a5bdb77df28d6a586bd0f14734f985e9
A-Ye311 <angelina.musik@live.de>
2026-06-16 22:30:46 +0200

    Dish API + insert_sample

--- app.py
+++ app.py
@@ -1,48 +1,72 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, jsonify
+from flask_bootstrap import Bootstrap5
+
 
 app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY = 'secret_key_just_for_dev_environment',
+    BOOTSTRAP_BOOTSWATCH_THEME = 'pulse'
+)
+
+from db import db, insert_sample
+from models import *
+
+bootstrap = Bootstrap5(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///dormanddine.db"
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 # landing page
 @app.route('/')
 def index():
-    return redirect(url_for('index'))
+    return "Coming soon", 200
 
 # feed von den meals
 @app.route('/feed')
 def feed():
-    return redirect(url_for('feed'))
+    dishes = db.session.execute(db.select(Dish).order_by(Dish.id)).scalars()
+    return render_template('feed.html', dishes=dishes)
 
 # einzelne Gerichte
-@app.route('/meal/<int:meal_id>')
-def meal_detail(meal_id):
-    return redirect(url_for('meal'))
+@app.route('/dishes/<int:dish_id>')
+def get_dish(dish_id):
+    dish = Dish.query.get_or_404(dish_id)
+    return jsonify({"id": dish.id, "name": dish.name, "description": dish.description, "price": dish.price, "left_portions": dish.left_portions, "status": dish.status})
 
 # Gerichte posten
 @app.route('/post', methods=['GET', 'POST'])
 def post_meal():
-    return redirect(url_for('post'))
+    return "Coming soon", 200
 
 # Profil anzeigen
 @app.route('/profile/<int:profil_id>')
-def profil():
-    return redirect(url_for('profile'))
+def profil(profil_id):
+    return "Coming soon", 200
 
 # Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return redirect(url_for('login'))
+    return "Coming soon", 200
 
 # Register
 @app.route('/register' , methods=['GET', 'POST'])
 def register():
-    return redirect(url_for('register'))
+    return "Coming soon", 200
 
 # Bestellübersicht
 @app.route('/order_view')
 def order_view():
-    return redirect(url_for('order_view'))
+    return "Coming soon", 200
 
-# 
+@app.route('/insert/sample')
+def run_insert_sample():
+    insert_sample()
+    return 'Database flushed and populated with some sample data.'
+
 @app.errorhandler(404)
 def http_not_found(e):
     return render_template('404.html'), 404
