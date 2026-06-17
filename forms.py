1d710de8d10005a4e8bd30652b6e9d8eaf9eedf8
juliadhwr <s_ng24@stud.hwr-berlin.de>
2026-06-17 20:36:00 +0200

    add logout to base.html

--- forms.py
+++ forms.py
@@ -5,22 +5,20 @@ from wtforms.validators import InputRequired, Length, EqualTo
 #https://flask-wtf.readthedocs.io/en/1.2.x/
 
 class LoginForm(FlaskForm):
-    email = StringField(validators=[InputRequired()])
-    password = PasswordField(validators=[InputRequired()])
+    email = StringField('E-Mail', validators=[InputRequired()])
+    password = PasswordField('Password',validators=[InputRequired()])
     login = SubmitField('Login')
 
 
 class RegisterForm(FlaskForm):
-    id = HiddenField()
-    first_name = StringField('Vorname', validators=[InputRequired(), Length(min=3)])
-    last_name = StringField('Nachname', validators=[InputRequired(), Length(min=3)])
+    first_name = StringField('First Name', validators=[InputRequired(), Length(min=3)])
+    last_name = StringField('Last Name', validators=[InputRequired(), Length(min=3)])
     username = StringField('Username', validators=[InputRequired(), Length(min=3)])
-    email = StringField('E-Mail-Adresse', validators=[InputRequired()])
-    password = PasswordField('Passwort', validators=[InputRequired(), Length(min=8)])
-    passwordagain = PasswordField('Wiederhole Passwort', validators=[InputRequired(), Length(min=8), EqualTo('password')]) 
-    phonenumber = StringField('Telefonnummer', validators=[InputRequired(), Length(min=5)])
-    is_cook = BooleanField('Ich habe Interesse für Dorm&Dine zu kochen')
-    dorm_id = SelectField('Wähle dein Wohnheim aus', choices=[(1, 'Wohnheim Nollendorfstraße')], coerce=int, validators=[InputRequired()])
-    bio = StringField('Schreibe etwas über dich')
-    created_at = HiddenField()
-    register = SubmitField('Registrieren')
+    email = StringField('E-Mail-address', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8)])
+    passwordagain = PasswordField('Repeat Password', validators=[InputRequired(), Length(min=8), EqualTo('password')]) 
+    phonenumber = StringField('Phone Number', validators=[InputRequired(), Length(min=5)])
+    is_cook = BooleanField('I am interested in Cooking for Dorm&Dine')
+    dorm_id = SelectField('Choose your dormitory', choices=[(1, 'Wohnheim Nollendorfstraße')], coerce=int, validators=[InputRequired()])
+    bio = StringField('Write a short Bio about yourself')
+    register = SubmitField('Registrate')
