f09b3b185591796860898c8a86dd9223cc8defc2
MrGreeeenTea <l-unger@gmx.de>
2026-06-17 20:37:48 +0200

    UI Grundgerüst ohne Datenbank Verbindung

--- app.py
+++ app.py
@@ -1,21 +1,35 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap5
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
 
 # landing page
 @app.route('/')
 def index():
-    return redirect(url_for('index'))
+    return redirect(url_for('feed'))
 
 # feed von den meals
 @app.route('/feed')
 def feed():
-    return redirect(url_for('feed'))
+    return render_template('feed.html')
 
 # einzelne Gerichte
 @app.route('/meal/<int:meal_id>')
 def meal_detail(meal_id):
-    return redirect(url_for('meal'))
+    return render_template('meal_detail.html')
+
+# einzelne Gerichte
+@app.route('/payment_success_cash')
+def payment_success_cash():
+    return render_template('payment_success_cash.html')
+
+# einzelne Gerichte
+@app.route('/payment_success_paypal')
+def payment_success_paypal():
+    return render_template('payment_success_paypal.html')
 
 # Gerichte posten
 @app.route('/post', methods=['GET', 'POST'])
@@ -23,7 +37,7 @@ def post_meal():
     return redirect(url_for('post'))
 
 # Profil anzeigen
-@app.route('/profile/<int: profil_id>')
+@app.route('/profile/<int:profil_id>')
 def profil():
     return redirect(url_for('profile'))
 
@@ -40,7 +54,7 @@ def register():
 # Bestellübersicht
 @app.route('/order_view')
 def order_view():
-    return redirect(url_for('order_view'))
+    return render_template('order_view.html')
 
 # 
 @app.errorhandler(404)
