a0f8bf7a2651be1c06f39b3d05f831e28bc2d60f
juliadhwr <s_ng24@stud.hwr-berlin.de>
2026-06-23 21:35:09 +0200

    change form selectfield to name of dorm not address

--- forms.py
+++ forms.py
@@ -19,7 +19,7 @@ class RegisterForm(FlaskForm):
     passwordagain = PasswordField('Repeat Password', validators=[InputRequired(), Length(min=8), EqualTo('password')]) 
     phonenumber = StringField('Phone Number', validators=[InputRequired(), Length(min=5)])
     is_cook = BooleanField('I am interested in Cooking for Dorm&Dine')
-    dorm_id = SelectField('Choose your dormitory', choices=[(1, 'Wohnheim Nollendorfstraße'),(2, 'Halbauer Weg 19-21'),(3, 'Franz-Mehring-Platz 2, 3')], coerce=int, validators=[InputRequired()])
+    dorm_id = SelectField('Choose your dormitory', choices=[(1, 'Wohnheim Nollendorfstraße'),(2, 'Wohnheim Halbauer Weg'),(3, 'Wohnheim Franz-Mehring-Platz')], coerce=int, validators=[InputRequired()])
     bio = StringField('Write a short Bio about yourself')
     register = SubmitField('Registrate')
 
@@ -39,7 +39,5 @@ class EditProfileForm(FlaskForm):
     
     new_bio = StringField('Update the short bio about yourself')
     new_phonenumber = StringField('Phone Number')
-    new_dorm_id = SelectField('Choose your new dormitory', choices=[(0, 'No dorm change'),(1, 'Wohnheim Nollendorfstraße'),(2, 'Halbauer Weg 19-21'),(3, 'Franz-Mehring-Platz 2, 3')], coerce=int)
+    new_dorm_id = SelectField('Choose your new dormitory', choices=[(0, 'No dorm change'),(1, 'Wohnheim Nollendorfstraße'),(2, 'Wohnheim Halbauer Weg'),(3, 'Wohnheim Franz-Mehring-Platz')], coerce=int)
     update = SubmitField('Update Profile')
-
-   
